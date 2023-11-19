ad505f380df661e14866e5d8a3076b8a943a6b53
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-11-20 02:22:56 +0530

    15-create item dtos

---
+++ src/main/java/com/possystem/possystemapi/dto/core/ItemDto.java
@@ -0,0 +1,16 @@
+package com.possystem.possystemapi.dto.core;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemDto {
+    private String code;
+    private String description;
+    private int qtyOnHand;
+    private double unitePrice;
+}
