4ab2c217391fac6ddd00b60ac39540ecd6edf774
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-11-20 01:20:11 +0530

    5-CustomerController saveCustomer Method initialize

--- src/main/java/com/possystem/possystemapi/controller/CustomerController.java
+++ src/main/java/com/possystem/possystemapi/controller/CustomerController.java
@@ -1,4 +1,31 @@
 package com.possystem.possystemapi.controller;
 
+import com.possystem.possystemapi.dto.requestDto.RequestCustomerDto;
+import com.possystem.possystemapi.dto.responseDto.ResponseCustomerDto;
+import com.possystem.possystemapi.service.CustomerService;
+import com.possystem.possystemapi.util.StandResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customer")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<StandResponse> save(@RequestBody RequestCustomerDto dto){
+        ResponseCustomerDto res = customerService.saveCustomer(dto);
+        return new ResponseEntity<>(
+                new StandResponse(200,"Customer saved",res), HttpStatus.CREATED
+        );
+    }
 }
