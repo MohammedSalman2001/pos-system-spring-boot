ccd40c802c576b5e2642fb6e8993cbc16f498afe
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-11-20 00:51:15 +0530

    Initial commit

---
+++ src/test/java/com/possystem/possystemapi/PosSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.possystem.possystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
