9d0739a7e32ba9e664a60ce6ef2831651d05582e
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-11-20 02:18:57 +0530

    14-create item entity

---
+++ src/main/java/com/possystem/possystemapi/entity/Item.java
@@ -0,0 +1,23 @@
+package com.possystem.possystemapi.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+public class Item {
+    @Id
+    private String code;
+    private String description;
+    private int qtyOnHand;
+    private double unitePrice;
+
+
+}
